d17ed47a0dac220c18b9acf25f7c61f3d18dfdf3
vincent wong <vincentwong687@gmail.com>
2021-12-01 10:04:30 +0800

    add i18n

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -7,6 +7,16 @@ import Container from '@components/Container.vue'
 <template>
   <Container>
     <Header></Header>
+    <div class="locale-changer">
+      <select v-model="$i18n.locale">
+        <option
+          v-for="locale in $i18n.availableLocales"
+          :key="`locale-${locale}`"
+          :value="locale"
+          >{{ locale }}</option
+        >
+      </select>
+    </div>
   </Container>
 
   <router-view />
