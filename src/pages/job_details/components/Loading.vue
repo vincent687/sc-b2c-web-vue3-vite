06a32e7d3d7e1906c97d4b27eda71cb2b5953368
B <philoman@gmail.com>
2021-12-07 12:52:06 +0800

    title and details page loading

---
+++ src/pages/job_details/components/Loading.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="border border-yellow-300 shadow rounded-md p-4 max-w-sm w-full mx-auto m-20"
+  >
+    <div class="animate-pulse flex flex-col space-x-4">
+      <div class="rounded-full bg-yellow-400 h-100 w-4" />
+      <div class="flex-1 space-y-4 py-1">
+        <div class="h-4 bg-yellow-400 rounded w-3/4" />
+        <div class="space-y-2">
+          <div class="h-4 bg-yellow-400 rounded" />
+          <div class="h-4 bg-yellow-400 rounded w-5/6" />
+        </div>
+      </div>
+      <div class="flex-1 space-y-4 py-1">
+        <div class="h-4 bg-yellow-400 rounded w-3/4" />
+        <div class="space-y-2">
+          <div class="h-4 bg-yellow-400 rounded" />
+          <div class="h-4 bg-yellow-400 rounded w-5/6" />
+        </div>
+      </div>
+      <div class="flex-1 space-y-4 py-1">
+        <div class="h-4 bg-yellow-400 rounded w-3/4" />
+        <div class="space-y-2">
+          <div class="h-4 bg-yellow-400 rounded" />
+          <div class="h-4 bg-yellow-400 rounded w-5/6" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
