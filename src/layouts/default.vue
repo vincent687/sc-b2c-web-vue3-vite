a0e4bf1e586032f1f7f14a7e201b3a5981ee98e4
B <philoman@gmail.com>
2021-12-07 13:22:44 +0800

    added basic transition animation demo

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -9,7 +9,29 @@
     <Header />
   </Container>
 
-  <router-view />
+  <router-view v-slot="{ Component, route }">
+    <transition name="slide-fade" mode="out-in">
+      <div :key="route.name">
+        <component :is="Component"></component>
+      </div>
+    </transition>
+  </router-view>
 
   <Footer />
 </template>
+
+<style>
+  .slide-fade-enter-active {
+    transition: all 0.2s ease;
+  }
+
+  .slide-fade-leave-active {
+    transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
+  }
+
+  .slide-fade-enter,
+  .slide-fade-leave-to {
+    transform: translateX(4px);
+    opacity: 0;
+  }
+</style>
