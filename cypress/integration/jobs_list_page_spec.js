3602765cfc3debbbe0a357b0b081d49dc6325c0c
vincent wong <vincentwong687@gmail.com>
2021-12-07 12:45:47 +0800

    test test:e2e:ci

--- cypress/integration/jobs_list_page_spec.js
+++ cypress/integration/jobs_list_page_spec.js
@@ -2,7 +2,7 @@ describe('Jobs page', () => {
   context('Visiting jobs page', () => {
     it('should editor choice section', () => {
       cy.log('Visiting jobs page')
-      cy.visit('/zh/jobs')
+      cy.visit('/')
 
       cy.log('Should see Editor Choices section')
       cy.findByText(/Editor Choices/i).should('be.visible')
