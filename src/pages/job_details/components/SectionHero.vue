29c18427617a2c553f91de92cc3ad65842a8793e
B <philoman@gmail.com>
2021-12-08 19:25:35 +0800

    fix style

--- src/pages/job_details/components/SectionHero.vue
+++ src/pages/job_details/components/SectionHero.vue
@@ -1,15 +1,20 @@
 <script setup lang="ts">
   import { defineProps } from 'vue'
   import { Job } from '@/contexts/job_details'
+  import Container from '@/components/Container.vue'
 
   defineProps<{ imageUrls: Job['imageUrls'] }>()
 </script>
 
 <template>
-  <div class="w-full h-52 sm:h-64 relative">
-    <router-link class="absolute top-6 left-6 bg-gray-300" :to="'/zh/jobs'">
-      BACK TO JOBS
-    </router-link>
+  <div class="w-full h-52 sm:h-64">
+    <Container>
+      <div class="relative">
+        <router-link class="absolute top-6 bg-gray-300" :to="'/zh/jobs'">
+          BACK TO JOBS
+        </router-link>
+      </div>
+    </Container>
     <img
       class="w-full h-full object-cover"
       alt="Good. For you."
